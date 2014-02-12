6a0bbf41c0e8b7e117f507bd467af2b63cf915fa
jacek.ciach@gmail.com <jacek.ciach@gmail.com@41554920-5288-75f3-eae7-e1c4e0c7abb4>
2014-02-12 22:01:14 +0000

    Creating v.0.1 tag

---
+++ tags/0.1/plugins/calendar/views/events.php
@@ -0,0 +1,43 @@
+<?php
+
+if (!defined('IN_CMS')) { exit(); }
+
+?>
+
+<h1><?php echo __('All events'); ?></h1>
+    <table class="index" cellspacing="0" cellpadding="0" border="0">
+        <thead>
+            <tr>
+                <td><?php echo __('Id'); ?></td>
+                <td><?php echo __('Title'); ?></td>
+                <td><?php echo __('Date from'); ?></td>
+                <td><?php echo __('Date to'); ?></td>                
+                <td><?php echo __('Created by'); ?></td>
+                <td><?php echo __('Delete'); ?></td>
+            </tr>
+        </thead>
+
+        <?php foreach($events as $event) { ?>
+
+        <tr class="<?php echo odd_even(); ?>">
+            <td><?php echo $event->getId(); ?></td>
+            <td><a href="<?php echo get_url('plugin/calendar/update/'.$event->id); ?>"><?php echo $event->getTitle(); ?></a></td>
+            <td><?php echo $event->getDateFrom(); ?></td>            
+            <td><?php echo $event->getDateTo(); ?></td>            
+            <td><?php echo $event->getCreator(); ?></td>
+            <td><a class="delete-event" href="<?php echo get_url('plugin/calendar/delete/'.$event->id); ?>"><img src="<?php echo ICONS_PATH; ?>action-delete-16.png" alt="Delete" /></a></td>
+        </tr>
+        <?php } ?>
+    </table>
+
+<script>
+  function onDeleteEventClick() {
+    var question = "<?php echo __("Are you sure you want to delete this event?"); ?>";
+    if (confirm(question))
+      return true;
+    else
+      return false;
+  }
+   
+  $("a.delete-event").click(onDeleteEventClick);  
+</script>
