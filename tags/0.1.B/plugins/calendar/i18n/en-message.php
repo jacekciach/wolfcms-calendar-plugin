0c48dc91c0845b3a383b43f5ff6df40ed8d19238
jacek.ciach@gmail.com <jacek.ciach@gmail.com@41554920-5288-75f3-eae7-e1c4e0c7abb4>
2014-02-13 13:10:20 +0000

    Creating v.0.1.B tag (includes website and update information in index.php)

---
+++ tags/0.1.B/plugins/calendar/i18n/en-message.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * English file for plugin calendar
+ *
+ */
+
+return array(
+    'Calendar'                                      => 'Calendar',
+    'Error occured during installing the calendar'  => 'Error occured during installing the Calendar Plugin',    
+    'Calendar is enabled!'                          => 'Calendar Plugin is enabled!',  
+    'Calendar is disabled!'                         => 'Calendar Plugin is disabled!',
+    'Calendar is uninstalled!'                      => 'Calendar Plugin is uninstalled!',
+    'Calendar is not uninstalled!'                  => 'Calendar Plugin is not uninstalled!',
+    'Documentation'                                 => 'Documentation',
+    'New event'                                     => 'New event',
+    'View all events'                               => 'View all events',
+    'The event has been successfully deleted'       => 'The event has been successfully deleted.',
+    'All events'                                    => 'All events',
+    'Id'                                            => 'Id',
+    'Title'                                         => 'Title',
+    'Description'                                   => 'Description',    
+    'Date from'                                     => 'Date',
+    'Date to'                                       => 'Date to',
+    'Created by'                                    => 'Created by',
+    'Edit'                                          => 'Edit',
+    'Delete'                                        => 'Delete',
+    'Posted by'                                     => 'Posted by',    
+    'to'                                            => 'to',
+    'days'                                          => 'days',
+    'Edit the event'                                => 'Edit the event',
+    'A new event'                                   => 'A new event',
+    'Event ID'                                      => 'Event ID',
+    'Could not update this event!'                  => 'Could not update this event!',
+    'There are errors in the form.'                 => 'There are errors in the form.',        
+    'The event has been updated.'                   => 'The event has been updated.',
+    'A new event has been created.'                 => 'A new event has been created.',
+    'Are you sure you want to delete this event?'   => 'Are you sure you want to delete this event?',    
+);
---
+++ tags/0.1.B/plugins/calendar/index.php
@@ -0,0 +1,79 @@
+<?php
+
+if (!defined('IN_CMS')) { exit(); }
+
+Plugin::setInfos(array(
+    'id'          => 'calendar',
+    'title'       => __('Calendar'),
+    'description' => __('Calendar'),
+    'version'     => '0.1',
+    'license'     => 'GPL',
+    'author'      => 'Jacek Ciach',
+    'require_wolf_version' => '0.7.3',
+    'website'     => 'https://code.google.com/p/wolfcms-calendar-plugin/',
+    'update_url'  => 'https://wolfcms-calendar-plugin.googlecode.com/svn/calendar-version.xml'
+));
+
+define('CALENDAR_VIEWS', 'calendar/views');
+
+Plugin::addController('calendar', __('Calendar'), 'admin_view', true);
+AutoLoader::addFile('CalendarEvent', CORE_ROOT.'/plugins/calendar/models/CalendarEvent.php');
+Behavior::add('calendar', 'calendar/behaviour.php');
+
+function showCalendar($slug, $date = null) {
+  $date_begin = new DateTime($date);
+  $date_begin->modify("first day of this month"); 
+  $date_begin->modify("-1 week");
+  $date_begin = $date_begin->format('Y-m-d');
+  
+  $date_end = new DateTime($date);
+  $date_end->modify("last day of this month"); 
+  $date_end->modify("+1 week");
+  $date_end = $date_end->format('Y-m-d');    
+  
+  $events = CalendarEvent::generateAllEventsBetween($date_begin, $date_end);
+  $events_map = array();
+  foreach ($events as $event) {
+    $events_map[$event->value][] = $event->getTitle();      
+  }
+  
+  $calendar = new View(
+                    PLUGINS_ROOT.DS.CALENDAR_VIEWS.'/calendar_table',
+                    array(
+                      'base_path' => BASE_URL.$slug,
+                      'date'      => $date,
+                      'map'       => $events_map
+                    ));
+  $calendar->display();
+}
+
+function showEvent($event, $show_author = true) {   
+  /* Prepare the event's data */
+  $vars['id']    = $event->getId();  
+  $vars['title'] = $event->getTitle();
+  
+  $date_from = new DateTime($event->getDateFrom());
+  $vars['date_from'] = strftime("%x", $date_from->getTimestamp());
+  
+  if (empty($event->date_to))
+    $vars['date_to'] = null;
+  else {
+    $date_to = new DateTime($event->getDateTo()); 
+    $vars['date_to'] = strftime("%x", $date_to->getTimestamp());
+  }
+  
+  $vars['days']    = $event->getLength();
+  $vars['author']  = $event->getCreator();
+  $vars['content'] = $event->getContent();
+  
+  $vars['show_author'] = $show_author;    
+ 
+  /* Display an event */
+  $view = new View(PLUGINS_ROOT.DS.CALENDAR_VIEWS.'/event_frontend', $vars);
+  $view->display();  
+}
+
+function showEvents(array $events) {
+  foreach ($events as $event)
+    showEvent($event);
+}
