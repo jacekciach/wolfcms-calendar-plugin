11c2066cc52095a44c452501e8f12e9ebfd1372f
jacek.ciach <jacek.ciach>
2014-07-01 15:52:33 +0000

    Added a Dutch traslation

---
+++ plugins/calendar/i18n/nl-message.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Dutch file for plugin calendar
+ *
+ */
+
+return array(
+    'Calendar'                                      => 'Kalender',
+    'Error occured during installing the calendar'  => 'Tijdens de instalatie van de Kalender plugin is er een fout opgetreden',    
+    'Calendar is enabled!'                          => 'Kalender Plugin is geactiveerd!',  
+    'Calendar is disabled!'                         => 'Kalender Plugin is gedeactiveerd!',
+    'Calendar is uninstalled!'                      => 'Kalender Plugin is gedeinstalleerd!',
+    'Calendar is not uninstalled!'                  => 'Kalender Plugin is niet geinstalleerd!',
+    'Documentation'                                 => 'Documentatie',
+    'New event'                                     => 'Nieuw evenement',
+    'View all events'                               => 'Alle evenementen',
+    'The event has been successfully deleted'       => 'Dit evenement is verwijderd.',
+    'All events'                                    => 'Alle evenementen',
+    'Id'                                            => 'Id',
+    'Title'                                         => 'Titel',
+    'Description'                                   => 'Beschrijving',    
+    'Date from'                                     => 'Datum',
+    'Date to'                                       => 'Datum tot',
+    'Created by'                                    => 'Aangemaakt door',
+    'Edit'                                          => 'Bewerken',
+    'Delete'                                        => 'Verwijderen',
+    'Posted by'                                     => 'Geplaatst door',    
+    'to'                                            => 'tot',
+    'days'                                          => 'dagen',
+    'Edit the event'                                => 'Evenement bewerken',
+    'A new event'                                   => 'Een nieuw evenement',
+    'Event ID'                                      => 'Evenement ID',
+    'Could not update this event!'                  => 'Evenement kon niet worden bijgewerkt!',
+    'There are errors in the form.'                 => 'Er zijn fouten in het formulier.',        
+    'The event has been updated.'                   => 'Dit evenement is bijgewerkt.',
+    'A new event has been created.'                 => 'Een nieuw evenement is aangemaakt.',
+    'Are you sure you want to delete this event?'   => 'Weet je zeker dat je dit evenement wilt verwijderen?',    
+);
+
+?>
